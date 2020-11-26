0a0d125ca8d8f1eb9d4b9b3b2778a9f5eb492654
MatheusFC2 <matheusfreitascampos@gmail.com>
2020-11-26 20:03:10 -0300

    ultimas atualizaçoes para aws

--- frontend/src/services/shortenerService.js
+++ frontend/src/services/shortenerService.js
@@ -1,22 +1,29 @@
 import baseAPI from './api';
 
-async function getLink(code) {
-  const result = await baseAPI.get(`links/${code}`);
-  return result.data;
-}
+class ShortenerService {
 
-async function getStats(code) {
-  const result = await baseAPI.get(`links/${code}/stats`);
-  return result.data;
-}
+  constructor() {
+    this.api = baseAPI(process.env.REACT_APP_API);
+  }
 
-async function postCreate(model) {
-  const result = await baseAPI.post('links', model);
-  return result.data;
-}
+  async function getLink(code) {
+    const result = await baseAPI.get(`links/${code}`);
+    return result.data;
+  }
 
-export default {
-  getLink,
-  getStats,
-  postCreate,
-};
+  async function getStats(code) {
+    const result = await baseAPI.get(`links/${code}/stats`);
+    return result.data;
+  }
+
+  async function postCreate(model) {
+    const result = await baseAPI.post('links', model);
+    return result.data;
+  }
+
+  export default {
+    getLink,
+    getStats,
+    postCreate,
+  };
+}
